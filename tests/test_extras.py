b714001c2df2a7acba6c2e427388f00b7d8daf85
DJ Passey <djpasseyjr@gmail.com>
2021-01-26 12:47:23 -0500

    Test nrmse and valid pred index

--- tests/test_extras.py
+++ tests/test_extras.py
@@ -3,10 +3,10 @@ import rescomp as rc
 
 class TestExtras(unittest.TestCase):
 
-    def test_accdur():
+    def test_accdur(self):
         pass
 
-    def test_relerr():
+    def test_relerr(self):
         x = np.array([3, 4])
         y = np.array([-2, -8])
         assert rc.relerr(x, y, order=2) == 13/5
@@ -16,8 +16,24 @@ class TestExtras(unittest.TestCase):
         assert np.all(rc.relerr(X, Y, order=2, axis=0) == np.array([1.25**.5 / 8**.5, 1.5 / 8**.5]))
         assert np.all(rc.relerr(X.T, Y.T, order="inf", axis=1) == np.array([1/2, 1.5/2]))
         
-    def test_sys_fit():
+    def test_sys_fit(self):
         pass
 
-    def test_train_test():
+    def test_train_test(self):
         pass
+    
+    def test_nrmse(self):
+        # Compare with results from R (hydroGOF package)
+        x = np.array([2,3,4,5])
+        y = np.array([2,3,5,5])
+        assert np.isclose(rc.nrmse(x,y), 0.387)
+        A = np.array([[2,3],[4,5]])
+        B = np.array([2,3],[5,5]])
+        assert np.isclose(rc.nrmse(A,B, axis=1), np.array([0, 1]))
+        assert np.isclose(rc.nrmse(A,B, axis=0), np.array([.5, 0]))
+        
+    def test_valid_prediction_index(self):
+        err = [0,0,0,0,0,0,0,1,0,0]
+        idx = rc.valid_prediction_index(err, .5)
+        assert idx == 7
+        
